09c63f12d0c3095fbfed0efc03f21a561503b9a8
Brian Helenbrook <bhelenbr@clarkson.edu>
2025-12-18 13:54:20 -0500

    Upgraded srs_scatter to scatter one big file

--- srs_scatter.py
+++ srs_scatter.py
@@ -10,6 +10,7 @@ import os
 import sys
 import platform
 from os.path import exists
+from make_cv.stringprotect import abbreviate_name
 
 new_column_names = {   
 'Proposal': 'Proposal_ID',
@@ -20,41 +21,42 @@ new_column_names = {
 'Long Descr': 'Title'
 }
 
-
-
-def merge_proposals(source,destination):
-	# dest_file = destination +"test.xlsx"
-	dest_file = destination
-	df_new = pd.read_excel(source,header=1,index_col='Proposal')
-	df_new.rename(columns=new_column_names, inplace=True)
-	df_new.drop(['Project','ID','PCT','RO Number'], axis=1,inplace=True)
+def merge_proposals(df_new,destination):
 	if exists(destination):
 		df_old = pd.read_excel(destination,index_col='Proposal_ID',sheet_name='Data')
-		
 		new_rows = df_new.loc[~df_new.index.isin(df_old.index)]
+		print(new_rows)
 		df_concat = pd.concat([df_old, new_rows])
 		#df_addPIs = pd.merge(df_concat, df_new['Principal Investigators'], left_index=True,right_index=True, how='left',suffixes=('_old',None))
 		#df_addPIs.drop(columns=['Principal Investigators_old'],inplace=True)
-		with pd.ExcelWriter(dest_file) as writer:
+		with pd.ExcelWriter(destination) as writer:
 			#df_addPIs.to_excel(writer,sheet_name='sheet1')
 			df_concat.to_excel(writer,sheet_name='Data')
 	else:
-		with pd.ExcelWriter(dest_file) as writer:
+		with pd.ExcelWriter(destination) as writer:
 			df_new.to_excel(writer,sheet_name='Data')
 	return
 
-source_folder = sys.argv[1]
-file_name = "proposals & grants.xlsx"
+
 # Read the desired source file, which is the updated Proposal and Grants file with new entries
+source = sys.argv[1]
+df = pd.read_excel(source,index_col='Proposal',skiprows=1)
+df.rename(columns=new_column_names, inplace=True)
+df.drop(['Project','ID','PCT','RO Number'], axis=1,inplace=True)
+df["Faculty"] = (
+	df["Faculty"]
+	.astype(str)
+	.apply(lambda x: abbreviate_name(x).lower())
+)
 
 faculty_folder = sys.argv[2]
-
 subfolder = "Proposals & Grants"
+file_name = "proposals & grants.xlsx"
 
 for FacultyName in os.listdir(faculty_folder):
-	if os.path.isdir(os.path.join(faculty_folder,FacultyName)):
-		lastname = FacultyName.lower()[0:FacultyName.find(",")];
-		print(lastname)
-		for file_to_append in os.listdir(source_folder):
-			if file_to_append.lower().find(lastname) != -1:
-				merge_proposals(source_folder +os.sep +file_to_append,faculty_folder+os.sep +FacultyName +os.sep +subfolder +os.sep +file_name)
+	pandg_folder = faculty_folder+os.sep +FacultyName +os.sep +subfolder
+	if os.path.isdir(pandg_folder):
+		name = abbreviate_name(FacultyName).lower();
+		print(name)
+		entries=df[df["Faculty"]==name]
+		merge_proposals(entries,faculty_folder+os.sep +FacultyName +os.sep +subfolder +os.sep +file_name)
